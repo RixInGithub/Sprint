397b74cc4e08c2f26c0278980f2f25c2919a9beb
RixTheTyrunt <87478581+RixInGithub@users.noreply.github.com>
2022-02-16 07:50:44 +0000

    Update files

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from gtts import gTTs as sprintspeech
+import pyttsx3
 import time
 import tkinter
 
-engine = sprintspeech.init()
+engine = pyttsx3.init()
 rate = engine.setProperty("rate", 60)
 print("Current rate: " + rate)
-""" Sprint face showing 1 starts """
-sprint1 = tkinter.Tk()
-sprint1.title("Sprint")
-sprintimage = PhotoImage(file="/sprint.png")
-sprintshow1 = (sprint1, image=sprintimage, borderwidth=0)
-sprint1.geometry("300x300")
-""" Sprint face showing 1 ends """
+# """ Sprint face showing 1 starts """
+# sprint1 = tkinter.Tk()
+# sprint1.title("Sprint")
+# sprintimage = PhotoImage(file="/sprint.png")
+# sprintshow1 = (sprint1, image=sprintimage, borderwidth=0)
+# sprint1.geometry("300x300")
+# """ Sprint face showing 1 ends """
 engine.say("Hi. I am Sprint.")
 engine.runAndWait()
 engine.stop()
 time.sleep(4)
-""" Sprint face hiding 1 starts """
-sprint1.destroy()
-""" Sprint face hiding 1 ends """
+# """ Sprint face hiding 1 starts """
+# sprint1.destroy()
+# """ Sprint face hiding 1 ends """
